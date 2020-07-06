0aa80efeed8a273ca46349b209f75cdf8896e93e
chukschiboy <chukschiboy@gmail.com>
2020-07-06 10:34:13 +0100

    update on the dashboard for the countries and numbers of cases

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,12 +1,51 @@
+import 'package:covid19_tracker/model/covid19_dashboard.dart';
+import 'package:covid19_tracker/services/networking.dart';
 import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({Key key}) : super(key: key);
-  
+
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  List<Covid19Dashboard> _list;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    // Networking network = Networking();
+    // List<Covid19Dashboard> result = network.getDashboardData();
+
+    // List<Covid19Dashboard> _list;
+
+    _list = getData();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: ListView.builder(itemBuilder: (context, index){
+        return ListTile(
+          title: Text('${_list[index].}'),
+        );
+      }),
     );
+  }
+
+  getData() async {
+    Networking network = Networking();
+    List<Covid19Dashboard> result = await network.getDashboardData();
+    setState(() {
+      _list = result;
+    });
+    // return result;
   }
 }
--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -2,9 +2,11 @@ import 'package:covid19_tracker/model/covid19_dashboard.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:covid19_tracker/model/serializers.dart';
+
 class Networking {
   
-   getDashboardData() async {
+   Future<List<Covid19Dashboard>> getDashboardData() async {
 
     List<Covid19Dashboard> _dashboardResult = [];
     var url = 'https://doh.saal.ai/api/live';
@@ -13,6 +15,12 @@ class Networking {
 
     if(response.statusCode == 200) {
       List<dynamic> list = jsonDecode(response.body);
+      _dashboardResult.addAll(list.map(
+        (e) => serializers.serializeWith(Covid19Dashboard.serializer, e)));
+    } else{
+      throw Exception('connection error');
     }
+
+    return _dashboardResult;
   }
 }
