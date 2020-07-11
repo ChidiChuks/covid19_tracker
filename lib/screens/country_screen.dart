bfba7153fc91fbed37bc41eb5aa39f3a28d9850d
chukschiboy <chukschiboy@gmail.com>
2020-07-11 11:20:12 +0100

    creating the single child scroll view for country_screen

--- lib/screens/country_screen.dart
+++ lib/screens/country_screen.dart
@@ -15,58 +15,60 @@ class CountryScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('${country.country} info.'),
       ),
-      body: Column(
-        // crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0,),
-            child: Image.network(
-              "https://www.countryflags.io/${country.countryCode}/flat/64.png",
-              // scale: 1.0,
-              width: MediaQuery.of(context).size.width / 2, 
-              fit: BoxFit.fill,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0,),
+              child: Image.network(
+                "https://www.countryflags.io/${country.countryCode}/flat/64.png",
+                // scale: 1.0,
+                width: MediaQuery.of(context).size.width / 3, 
+                fit: BoxFit.fill,
+              ),
             ),
-          ),
 
-          SizedBox(height: 10),
+            SizedBox(height: 10),
 
-          Container(
-            width: MediaQuery.of(context).size.width - 10,
-            height: (MediaQuery.of(context).size.width * 0.70) - 10,
-            child: SfCartesianChart(
-              primaryXAxis: CategoryAxis(),
-              // Chart title
-              title: ChartTitle(text: 'Half yearly analysis'),
-              // Enable legend
-              legend: Legend(isVisible: true),
-              // Enable tooltip
-              tooltipBehavior: TooltipBehavior(enable: true),
-              series: <ChartSeries<SalesData, String>>[
-                LineSeries<SalesData, String>(
-                    dataSource: <SalesData>[
-                      SalesData('Jan', 35),
-                      SalesData('Feb', 28),
-                      SalesData('Mar', 34),
-                      SalesData('Apr', 32),
-                      SalesData('May', 40)
-                    ],
-                    xValueMapper: (SalesData sales, _) => sales.year,
-                    yValueMapper: (SalesData sales, _) => sales.sales,
-                    // Enable data label
-                    dataLabelSettings: DataLabelSettings(isVisible: true))
-              ]),
-          ),
+            Container(
+              width: MediaQuery.of(context).size.width - 10,
+              height: (MediaQuery.of(context).size.width * 0.70) - 10,
+              child: SfCartesianChart(
+                primaryXAxis: CategoryAxis(),
+                // Chart title
+                title: ChartTitle(text: 'Half yearly analysis'),
+                // Enable legend
+                legend: Legend(isVisible: true),
+                // Enable tooltip
+                tooltipBehavior: TooltipBehavior(enable: true),
+                series: <ChartSeries<SalesData, String>>[
+                  LineSeries<SalesData, String>(
+                      dataSource: <SalesData>[
+                        SalesData('Jan', 35),
+                        SalesData('Feb', 28),
+                        SalesData('Mar', 34),
+                        SalesData('Apr', 32),
+                        SalesData('May', 40)
+                      ],
+                      xValueMapper: (SalesData sales, _) => sales.year,
+                      yValueMapper: (SalesData sales, _) => sales.sales,
+                      // Enable data label
+                      dataLabelSettings: DataLabelSettings(isVisible: true))
+                ]),
+            ),
 
-          // buildDetailText(color: Colors.orangeAccent, count: index + 1, text: 'Ranks'),
+            // buildDetailText(color: Colors.orangeAccent, count: index + 1, text: 'Ranks'),
 
-          buildDetailText(color: Colors.orangeAccent, count: country.confirmed, text: 'Confirmed'),
-                          
-          buildDetailText(color: Colors.blue, count: country.active, text: 'Active'),
-          
-          buildDetailText(color: Colors.green, count: country.recovered, text: 'Recovered'),
-          
-          buildDetailText(color: Colors.red, count: country.deaths, text: 'Deaths'),
-        ],
+            buildDetailText(color: Colors.orangeAccent, count: country.confirmed, text: 'Confirmed'),
+                            
+            buildDetailText(color: Colors.blue, count: country.active, text: 'Active'),
+            
+            buildDetailText(color: Colors.green, count: country.recovered, text: 'Recovered'),
+            
+            buildDetailText(color: Colors.red, count: country.deaths, text: 'Deaths'),
+          ],
+        ),
       ),
     );
   }
