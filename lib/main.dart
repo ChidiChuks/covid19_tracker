25553a87556815e5259101c782177f1c2a8620f8
chukschiboy <chukschiboy@gmail.com>
2020-07-11 12:51:33 +0100

    creating the homescreen with icons, clipPath and images to prevent and educate about the pandemic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:covid19_tracker/bloc/global_bloc.dart';
+import 'package:covid19_tracker/constant.dart';
 import 'package:covid19_tracker/screens/dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_core/core.dart';
 
@@ -23,12 +25,89 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Covid-19 Tracker',
-      // debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        // primarySwatch: Colors.blue[900],
+       scaffoldBackgroundColor: kBackgroundColor,
+       fontFamily: "Poppins",
+       textTheme: TextTheme(body1: TextStyle(color: kBodyTextColor)),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: DashboardScreen(),
+      // home: DashboardScreen(),
+      home: HomeScreen(),
     );
   }
 }
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          ClipPath(
+            clipper: MyClipper(),
+            child: Container(
+              padding: EdgeInsets.only(left: 40, top: 50, right: 20),
+              height: 350,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xFF3383CD),
+                    Color(0xFF11249F),
+                  ],
+                ),
+                image: DecorationImage(
+                  image: AssetImage("assets/images/virus.png"),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: SvgPicture.asset("assets/icons/menu.svg")
+                    ),
+                    SizedBox(height: 20,),
+                    Expanded(
+                      child: Stack(
+                        children: <Widget>[
+                          SvgPicture.asset(
+                            "assets/icons/Drcorona.svg", 
+                            width: 230, 
+                            fit: BoxFit.fitWidth, 
+                            alignment: Alignment.topCenter,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path>{
+   @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(size.width / 2, size.height, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+ 
